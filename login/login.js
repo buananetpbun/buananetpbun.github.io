17472a73275ba36ea5e72850df524710d3a687fd
Agus Ramadhani <42666125+buananetpbun@users.noreply.github.com>
2022-10-28 01:21:22 +0700

    Update login.js

--- login/login.js
+++ login/login.js
@@ -341,7 +341,7 @@ document
 	.addEventListener("click", () => {
 		const email = document.querySelector("#login-email").value;
 		if (email.trim() == "") {
-			alert("Enter Your E-mail.");
+			customAlert.alert("Enter Your E-mail.");
 		} else {
 			forgotPassword(email);
 		}
@@ -357,3 +357,37 @@ const forgotPassword = (email) => {
 			alert("invalid e-mail or bad network connection.");
 		});
 }
+
+function CustomAlert(){
+  this.alert = function(message,title){
+    document.body.innerHTML = document.body.innerHTML + '<div id="dialogoverlay"></div><div id="dialogbox" class="slit-in-vertical"><div><div id="dialogboxhead"></div><div id="dialogboxbody"></div><div id="dialogboxfoot"></div></div></div>';
+
+    let dialogoverlay = document.getElementById('dialogoverlay');
+    let dialogbox = document.getElementById('dialogbox');
+    
+    let winH = window.innerHeight;
+    dialogoverlay.style.height = winH+"px";
+    
+    dialogbox.style.top = "100px";
+
+    dialogoverlay.style.display = "block";
+    dialogbox.style.display = "block";
+    
+    document.getElementById('dialogboxhead').style.display = 'block';
+
+    if(typeof title === 'undefined') {
+      document.getElementById('dialogboxhead').style.display = 'none';
+    } else {
+      document.getElementById('dialogboxhead').innerHTML = '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> '+ title;
+    }
+    document.getElementById('dialogboxbody').innerHTML = message;
+    document.getElementById('dialogboxfoot').innerHTML = '<button class="pure-material-button-contained active" onclick="customAlert.ok()">OK</button>';
+  }
+  
+  this.ok = function(){
+    document.getElementById('dialogbox').style.display = "none";
+    document.getElementById('dialogoverlay').style.display = "none";
+  }
+}
+
+let customAlert = new CustomAlert();
